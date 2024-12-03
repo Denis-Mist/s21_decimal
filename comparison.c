3be47b9ce0401982a7f718ccf0cd194793fec037
Your Name <gilmajac@DESKTOP-PKMT05F.localdomain>
2024-12-03 12:23:14 +0400

    fixed comparison

--- comparison.c
+++ comparison.c
@@ -7,7 +7,6 @@ typedef struct
     int bits[4];
 } s21_decimal;
 
-int abs(int m);
 int s21_is_equal(s21_decimal num1, s21_decimal num2);
 int s21_is_not_equal(s21_decimal num1, s21_decimal num2);
 int s21_is_less(s21_decimal num1, s21_decimal num2);
@@ -20,39 +19,39 @@ int s21_negate(s21_decimal value, s21_decimal *result);
 void set_sign(s21_decimal *num, int sign);
 void set_scale(s21_decimal *num, int scale);
 void decimal_shift_left(s21_decimal *num, int shift);
-void s21_add(s21_decimal one_shift, s21_decimal three_shift, s21_decimal *result);
+void decimal_shift_right(s21_decimal *num, int shift);
+int s21_add(s21_decimal one_shift, s21_decimal three_shift, s21_decimal *result);
 void increase_scale(s21_decimal *num, int delta);
 void decrease_scale(s21_decimal *num, int delta);
 void s21_print_decimal(s21_decimal decimal);
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
+void s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
 void set_same_scale(s21_decimal *tmp1, s21_decimal *tmp2);
+int check_for_verylow(s21_decimal *num, int new_scale);
 
 int main() {
-    s21_decimal num1 = {1700000000, 0, 0, 0};
-    s21_decimal num2 = {17000, 0, 0, 0};
+    s21_decimal num1 = {2000, 0, 0, 0};
+    s21_decimal num2 = {5000, 0, 0, 0};
     s21_decimal res = { 0 };
-
+    
     // set_scale(&num1, 10);
     printf("value_1: ");
-    set_scale(&num1, 6);
+    set_scale(&num1, 14);
     s21_print_decimal(num1);
     printf("value_2: ");
-    set_scale(&num2, 1);
+    set_scale(&num2, 19);
     s21_print_decimal(num2);
-    printf("%d\n", s21_is_equal(num1, num2));
+    // printf("%d\n", s21_is_equal(num1, num2));
     // set_scale(&num1, 15);
     // decrease_scale(&num1, 1);
-    // printf("result:\n");
-    // printf("sign: %d, scale: %d\n", get_sign(num1), get_scale(num1));
-    // s21_print_decimal(num1);
+    int fault = s21_mul(num1, num2, &res);
+    printf("result: %d\n", fault);
+    printf("sign: %d, scale: %d\n", get_sign(res), get_scale(res));
+	//printf("%d\n", s21_is_equal(num1, num2));
+    s21_print_decimal(res);
+
     //if (strcmp("", "") == 0) printf("Success\n");
     return 0;
-}
-
-int abs(int m) {
-    if (m < 0) m = -m;
-    return m;
 }
 
 int s21_is_equal(s21_decimal num1, s21_decimal num2) {
@@ -60,9 +59,10 @@ int s21_is_equal(s21_decimal num1, s21_decimal num2) {
     if (get_sign(num1) != get_sign(num2)) res = 0;
     s21_decimal tmp1 = num1;
     s21_decimal tmp2 = num2;
-    set_same_scale(&tmp1, &tmp2);
+    if (get_scale(tmp1) != get_scale(tmp2)) set_same_scale(&tmp1, &tmp2);
     for (int i = 0; i < 4 && res == 1; i++) {
-        if (tmp1.bits[i] != tmp2.bits[i]) res = 0;
+        if (((tmp1.bits[i] << 31) & 1) != ((tmp1.bits[i] << 31) & 1)) res = 0;
+        else if (tmp1.bits[i] != tmp2.bits[i]) res = 0;
     }
     return res;
 }
@@ -79,9 +79,11 @@ int s21_is_less(s21_decimal num1, s21_decimal num2) {
     else if (get_sign(num1) > get_sign(num2)) res = 0;
     s21_decimal tmp1 = num1;
     s21_decimal tmp2 = num2;
-    set_same_scale(&tmp1, &tmp2);
+    if (get_scale(tmp1) != get_scale(tmp2)) set_same_scale(&tmp1, &tmp2);
     for (int i = 2; i >= 0 && res == -1; i--) {
-        if (tmp1.bits[i] < 0 && tmp2.bits[i] >= 0) res = 0;
+		if (((tmp1.bits[i] << 31) & 1) > ((tmp1.bits[i] << 31) & 1)) res = 0;
+		else if (((tmp1.bits[i] << 31) & 1) < ((tmp1.bits[i] << 31) & 1)) res = 1;
+        else if (tmp1.bits[i] < 0 && tmp2.bits[i] >= 0) res = 0;
         else if (tmp1.bits[i] >= 0 && tmp2.bits[i] < 0) res = 1;
         else if (tmp1.bits[i] < tmp2.bits[i]) res = 1;
         else if (tmp1.bits[i] > tmp2.bits[i]) res = 0;
@@ -187,10 +189,31 @@ void decimal_shift_left(s21_decimal *num, int shift) {
     num->bits[2] |= buffer2;
 }
 
+void decimal_shift_right(s21_decimal *num, int shift) {
+    int buffer1 = 0;
+    for (int i = 0; i < shift; i++) {
+        buffer1 = buffer1 << 1;
+        buffer1 |= (num->bits[2] >> i) & 1;
+    }
+    num->bits[2] = num->bits[2] >> shift;
+    int buffer2 = 0;
+    for (int i = 0; i < shift; i++) {
+        buffer2 = buffer2 << 1;
+        buffer2 |= (num->bits[1] >> 31) & 1;
+    }
+    num->bits[1] = num->bits[1] << 31 - shift;
+    num->bits[1] |= buffer1;
+    num->bits[0] = num->bits[0] << 31 - shift;
+    num->bits[0] |= buffer2;
+}
 
-// Целое + целое
-void s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+// Без учета отрицательных и ошибок
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int res = 0;
     int buffer = 0;
+    s21_decimal tmp1 = value_1;
+    s21_decimal tmp2 = value_2;
+    set_same_scale(&tmp1, &tmp2);
     for (int i = 0; i < 3; i++) {
         result->bits[i] = 0;
         for (int j = 0; j <= 31; j++) {
@@ -207,6 +230,12 @@ void s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
             } else buffer = 0;
         }
     }
+    set_scale(result, get_scale(tmp1));
+    if (buffer == 1 && get_scale(*result) > 0) {
+        decimal_shift_right(result, 1);
+        result->bits[2] |= (1 << 31);
+    } else if (buffer == 1 && get_scale(*result) == 0) res = 1;
+    return res;
 }
 
 void s21_print_decimal(s21_decimal decimal) {
@@ -218,16 +247,21 @@ void s21_print_decimal(s21_decimal decimal) {
     printf("\n");
 }
 
-// Лучшее на данный момент (умножение)
+// Без обработки ошибок
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int res = 0;
+    int shift = 0;
     s21_decimal buffer_dec = { 0 };
-    set_scale(result, get_scale(value_1) + get_scale(value_2));
+    s21_decimal tmp1 = value_1;
+    s21_decimal tmp2 = value_2;
+    int new_scale = get_scale(value_1) + get_scale(value_2);
     if ((get_sign(value_1) == -1 && get_sign(value_2) == 1) || (get_sign(value_1) == 1 && get_sign(value_2) == -1)) set_sign(result, -1);
     // берем каждый бит value_2
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j <= 31; j++) {
+    for (int i = 0; i < 3 && res == 0; i++) {
+        for (int j = 0; j <= 31 && res == 0; j++) {
             int bit2 = (value_2.bits[i] >> j) & 1;
             s21_decimal buffer_line = { 0 };
+            if (bit2 == 1) shift = 0;
             // берем каждый бит value_1
             for (int m = 0; m < 3 && bit2 == 1; m++) {
                 for (int n = 0; n <= 31; n++) {
@@ -235,17 +269,33 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
                     buffer_line.bits[m] |= (bit1 << n);
                 }
             }
-            decimal_shift_left(&buffer_line, j + 31*i);
+            int k = 0;
+            while (k < (j + 31*i) && !((buffer_line.bits[2] << 31) & 1)) {
+                decimal_shift_left(&buffer_line, 1);
+                k++;
+            }
+            if (shift == 0 && k < (j + 31*i)) {
+                shift = (j + 31*i) - k;
+                decimal_shift_right(&buffer_dec, (j + 31*i) - k);
+            } else if (k < (j + 31*i)) {
+                shift++;
+                decimal_shift_right(&buffer_dec, 1);
+            }
             s21_add(buffer_line, buffer_dec, result);
             buffer_dec = *result;
         }
     }
-    *result = buffer_dec;
+    if (new_scale > 28 && res == 0 && check_for_verylow(&buffer_dec, new_scale - 28)) res = 2;
+    if (new_scale <= 28) set_scale(&buffer_dec, new_scale);
+    else set_scale(&buffer_dec, 28);
+    *result = buffer_dec; 
+    return res;
 }
 
 // Уменьшение дробной части на delta знаков (деление на 10 в delta степени)
 void decrease_scale(s21_decimal *num, int delta) {
     int k = 0;
+	int zero;
     while (k < delta) {
         s21_decimal buffer1 = { 0 };
         s21_decimal buffer2 = { 0 };
@@ -254,15 +304,17 @@ void decrease_scale(s21_decimal *num, int delta) {
         int j = 93;
         int i = 2;
         // Получаем первое делимое
-        while (s21_is_less(buffer1, del)) {
+        while (buffer1.bits[0] < del.bits[0] && j >= 0) {
             decimal_shift_left(&buffer1, 1);
             buffer1.bits[0] |= (num->bits[i] >> (j - 31*i)) & 1;
             j--;
             if (j % 31 == 0) i--;
         }
+		if (buffer1.bits[0] == 0 && buffer1.bits[1] == 0 && buffer1.bits[2] == 0) zero = 1;
+		else zero = 0;
         // Деление в столбик
-        while (j >= 0) {
-            if (!s21_is_less(buffer1, del)) {
+        while (j >= 0 && zero == 0) {
+            if (buffer1.bits[0] >= del.bits[0]) {
                 res.bits[0] |= 1;
                 s21_sub(buffer1, del, &buffer2);
                 buffer1 = buffer2;
@@ -275,7 +327,7 @@ void decrease_scale(s21_decimal *num, int delta) {
                 if (s21_is_less(buffer1, del) && j != 0) decimal_shift_left(&res, 1);
             }
         }
-        if (!s21_is_less(buffer1, del)) res.bits[0] |= 1;
+        if (!s21_is_less(buffer1, del) && zero == 0) res.bits[0] |= 1;
         for (int m = 0; m < 3; m++) num->bits[m] = res.bits[m];
         k++;
     }
@@ -283,14 +335,13 @@ void decrease_scale(s21_decimal *num, int delta) {
 }
 
 // Большее целое - меньшее целое
-int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+void s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     int buffer = 0;
-    
     for (int i = 0; i < 3; i++) {
         result->bits[i] = 0;
         for (int j = 0; j <= 31; j++) {
-            int num1 = (value_1.bits[i] >> j) & 1;
-            int num2 = (value_2.bits[i] >> j) & 1;
+            int num1 = ((value_1.bits[i] >> j) & 1);
+            int num2 = ((value_2.bits[i] >> j) & 1);
             if ((num1 - num2 - buffer) == 1) {
                 result->bits[i] |= (1 << j);
                 buffer = 0;
@@ -307,9 +358,9 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 }
 
 void set_same_scale(s21_decimal *tmp1, s21_decimal *tmp2) {
-    if (get_scale(*tmp1) != get_scale(*tmp2)) {
+    if ((get_scale(*tmp1)) != (get_scale(*tmp2))) {
         int delta = get_scale(*tmp1) - get_scale(*tmp2);
-        while (delta < 0 && (((tmp1->bits[2]) >> 31) & 1) == 0 && get_scale(*tmp1) < 28) {
+        while (delta < 0 && (((tmp1->bits[2]) >> 31) & 1) == 0 && (get_scale(*tmp1) < 28)) {
             increase_scale(tmp1, 1);
             delta++;
         }
@@ -317,7 +368,7 @@ void set_same_scale(s21_decimal *tmp1, s21_decimal *tmp2) {
             decrease_scale(tmp2, 1);
             delta++;
         }
-        while (delta > 0 && (((tmp2->bits[2]) >> 31) & 1) == 0 && get_scale(*tmp2) < 28) {
+        while (delta > 0 && (((tmp2->bits[2]) >> 31) & 1) == 0 && (get_scale(*tmp2) < 28)) {
             increase_scale(tmp2, 1);
             delta--;
         }
@@ -326,4 +377,15 @@ void set_same_scale(s21_decimal *tmp1, s21_decimal *tmp2) {
             delta--;
         }
     }
+}
+
+int check_for_verylow(s21_decimal *num, int delta_scale) {
+    int verylow = 1;
+	decrease_scale(num, delta_scale);
+	for (int i = 2; i >= 0 && verylow == 1; i--) {
+		for (int j = 0; j < 32 && verylow == 1; j++) {
+			if ((num->bits[i] >> j) & 1) verylow = 0;
+		}
+	}
+	return verylow;
 }
